ac8eda32eb70301bc2a43be343e882f05c61d7ea
Takahiro Uchiyama <hochweit728@gmail.com>
2015-09-19 16:00:21 +0900

    Initial commit

---
+++ lib/wallet.rb
@@ -0,0 +1,18 @@
+class wallet
+  attr_accessor \
+    :one, :five, :ten, :fifty, :hundred, :five_hundred,
+    :thousand, :five_thousand, :ten_thousand
+
+  def initialize(one: 0, five: 0, ten: 0, fifty: 0, hundred: , five_hundred:)
+    
+  end
+
+  def amount
+  end
+
+  def deposit
+  end
+
+  def withdraw
+  end
+end
