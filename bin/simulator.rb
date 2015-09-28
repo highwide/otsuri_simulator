2eff02f3d21dd707423b978c58b177b73622687d
Takahiro Uchiyama <hochweit728@gmail.com>
2015-09-28 17:29:06 +0900

    Modify the simulator

--- bin/simulator.rb
+++ bin/simulator.rb
@@ -3,5 +3,12 @@ Bundler.require
 
 require './lib/lazy_person.rb'
 
-person = LazyPerson.new
-binding.pry
+ave = []
+10000.times do 
+  person = LazyPerson.new
+  person.do_shopping
+  ave << person.coins_history.inject{|sum, item| sum + item}.to_f / person.coins_history.length.to_f
+end
+
+puts ""
+puts "平均: #{ave.inject{|sum, item| sum + item}.to_f / ave.length.to_f}枚の硬貨"
